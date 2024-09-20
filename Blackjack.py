6c825336a7435f567446efa05af82f462987b405
Melanie Irigoyen <a7149@palmore.edu.mx>
2024-09-19 23:13:13 -0600

    actualización de código

--- Blackjack.py
+++ Blackjack.py
@@ -1,12 +1,15 @@
 import random
-def tenercartas():
+''' Función para la entrega de cartas al azar'''
+def tenercartas(): 
     cartasowo = [2,3,4,5,6,7,8,9,10,10,10,10,11]
     return random.choice(cartasowo)
 
 def calcularpuntaje(cartasowo):
+    '''Función para calcular el puntaje que se lleva con cada carta que obtienes'''
     valor = sum(cartasowo)
     if valor > 21 and 11 in cartasowo:
         for i in range(len(cartasowo)):
+            ''' Aquí se checa si el puntaje es menor a 21 o 11, el as vale 1'''
             if cartasowo[i] == 11:
                 cartasowo[i] = 1
                 valor = sum(cartasowo)
@@ -16,6 +19,7 @@ def calcularpuntaje(cartasowo):
 def juego21():
     print("¡Hola! Bienvenido al juego Blackjack ¿estás listo para comenzar?")
     cartasP1 = []
+    '''Se repite hasta que el jugador diga que ya no quiere cartas o supere los 21 puntos'''
     while True:
         respuesta = input("¿Quieres una carta? owo -pon 'si' o 'no'- ")
 
@@ -35,12 +39,15 @@ def juego21():
         else:
             print("Lo que ingresaste es incorrecto, responde 'si' o 'no'")
 
+    '''Esto es para generar el puntaje de la casa'''
     puntajecasa = random.randint(17, 26)
     puntajeP1 = calcularpuntaje(cartasP1)
 
+    '''Aquí dice cuánto puntaje tiene cada uno (jugador y casa)'''
     print(f"Te has detenido al tener un puntaje de {puntajeP1}")
     print(f"La casa tiene un puntaje de {puntajecasa}")
 
+    '''Aquí son las condicionales para saber cuando el jugador pierde, gana o hay empate'''
     if puntajeP1 == 21 and puntajecasa < 21:
         print("¡Enhorabuena! Has ganado con un 21 uwu")
     elif puntajecasa > 21 and puntajeP1 <= 21:
@@ -58,8 +65,4 @@ def juego21():
         print("Repámpanos, has perdido :(")
 
 juego21()
-
-
-''' Aun no lo termino, me falta el readme y quiero acompletarlo con lo de hacer 
-las funciones a parte y otras cositas, también el explicar todo :('''
 
